7ef3f55f9a79ebbeba9a61d8513fca7a82a48de9
Kevin Quinn <kpquinn2@wisc.edu>
2018-02-03 15:41:43 -0600

    AWS provisions from ~/.aws/credentials file automatically; unnecessary work

--- go-api/dynamo.go
+++ go-api/dynamo.go
@@ -3,17 +3,12 @@ package main
 import (
     "os"
     "fmt"
-    "github.com/go-ini/ini"
     "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/aws/credentials"
     "github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 var (
-    aws_cred_file = os.Getenv("HOME") + "/.aws/credentials"
-    id_string = "aws_access_key_id"
-    key_string = "aws_secret_access_key"
     local_env = "LOCAL_DYNAMODB"
     region = "us-east-2"
 )
@@ -27,43 +22,14 @@ func check(e error) {
 }
 
 
-func getCredsFromFile() *credentials.Credentials {
-    if _, err := os.Stat(aws_cred_file); os.IsNotExist(err) {
-        fmt.Printf("%s does not exist\n", aws_cred_file)
-        os.Exit(1)
-    }
-
-    file, err := ini.Load(aws_cred_file)
-    check(err)
-
-    profile, err := file.GetSection("default")
-    check(err)
-
-    id_string_key := profile.Key(id_string).String()
-    key_string_key := profile.Key(key_string).String()
-    return credentials.NewStaticCredentials(id_string_key,
-                                            key_string_key, "")
-}
-
-
-func useAWS() *dynamodb.DynamoDB {
+func useAWS(sess *session.Session) *dynamodb.DynamoDB {
     fmt.Println("Using AWS DynamoDB connection...")
-    creds := getCredsFromFile()
-    sess, err := session.NewSession(&aws.Config{
-                Region: aws.String(region),
-                Credentials: creds,
-                })
-    check(err)
-
     return dynamodb.New(sess)
 }
 
 
-func useLocal() *dynamodb.DynamoDB {
+func useLocal(sess *session.Session) *dynamodb.DynamoDB {
     fmt.Println("Using local DynamoDB connection...")
-    sess, err := session.NewSession(&aws.Config{Region: aws.String(region)})
-    check(err)
-
     //backslash might be necessary https://stackoverflow.com/questions/33801460/dynamo-db-local-connection-refused#37106007
     return dynamodb.New(sess, aws.NewConfig().WithEndpoint("http://localhost:8000/"))
 }
@@ -71,11 +37,13 @@ func useLocal() *dynamodb.DynamoDB {
 
 func main() {
     var svc *dynamodb.DynamoDB
+    sess, err := session.NewSession(&aws.Config{Region: aws.String(region)})
+    check(err)
 
     if os.Getenv(local_env) == "1" {
-        svc = useLocal()
+        svc = useLocal(sess)
     } else {
-        svc = useAWS()
+        svc = useAWS(sess)
     }
 
     input := &dynamodb.ListTablesInput{}
