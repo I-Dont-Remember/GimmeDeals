a7204dc0ca837d5ba809ea3f854d9e1ce95f7eb0
Kevin Quinn <kpquinn2@wisc.edu>
2018-02-10 17:58:29 -0600

    Correctly returns the json array of deals to lambda logs

--- go-api/lambda/main.go
+++ go-api/lambda/main.go
@@ -1,64 +1,101 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "github.com/aws/aws-lambda-go/lambda"
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/dynamodb"
-)
+	"log"
+	"os"
 
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+)
 
 var (
-    region = "us-east-2"
-    output *dynamodb.ListTablesOutput
+	region    = "us-east-2"
+	output    *dynamodb.ScanOutput
+	tableName = "Deals"
 )
 
-
 type Request struct {
-    id int `json:"id"`
+	id int `json:"id"`
 }
 
+type Deal struct {
+	Id       string `json:"Id"`
+	Day      string `json:"Day"`
+	Location string `json:"Location"`
+	Deal     string `json:"Deal"`
+}
+
+func buildParams() *dynamodb.ScanInput {
+	filt := expression.Name("Id").AttributeExists()
+	proj := expression.NamesList(expression.Name("Id"),
+		expression.Name("Day"),
+		expression.Name("Location"),
+		expression.Name("Deal"))
+
+	expr, err := expression.NewBuilder().WithFilter(filt).WithProjection(proj).Build()
+	if err != nil {
+		log.Print("Error building expression...")
+		os.Exit(2)
+	}
+
+	params := &dynamodb.ScanInput{
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		FilterExpression:          expr.Filter(),
+		ProjectionExpression:      expr.Projection(),
+		TableName:                 aws.String(tableName),
+	}
+	return params
+}
 
 func init() {
-    sess, err := session.NewSession(&aws.Config{Region: aws.String(region)})
-    if err != nil {
-        log.Print("Error getting session")
-        log.Print(err)
-        // this might not be good inside lambda function???
-        os.Exit(2)
-    }
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(region)})
+	if err != nil {
+		log.Print("Error getting session")
+		log.Print(err)
+		// this might not be good inside lambda function???
+		os.Exit(2)
+	}
 
-    svc := dynamodb.New(sess)
+	svc := dynamodb.New(sess)
+	params := buildParams()
 
-    input := &dynamodb.ListTablesInput{}
+	result, err := svc.Scan(params)
+	if err != nil {
+		log.Print("Error scanning...")
+		os.Exit(2)
+	}
 
-    result, err := svc.ListTables(input)
-    if err != nil {
-        log.Print("Error listing tables")
-        log.Print(err)
-        os.Exit(2)
-    }
-
-    output = result
+	output = result
 }
 
+// Handler processes the DynamoDB query response and returns formatted json
+func Handler(request Request) ([]Deal, error) {
+	log.Print("Fetching Deals...")
+	var deals []Deal
 
-func Handler(request Request) ([]string, error) {
-    log.Print("Running Deals...")
-    log.Print(output)
+	for _, i := range output.Items {
+		deal := Deal{}
 
-    var names []string
-    for _, pointer := range output.TableNames {
-        fmt.Println(*pointer)
-        names = append(names,*pointer)
-    }
-    return names, nil
+		err := dynamodbattribute.UnmarshalMap(i, &deal)
+
+		if err != nil {
+			log.Print("Error unmarshaling item...")
+			os.Exit(2)
+		}
+
+		log.Print(deal)
+		deals = append(deals, deal)
+	}
+	// No need to marshall into json, already handled by aws-sdk-go
+	return deals, nil
 }
-
 
 func main() {
-    lambda.Start(Handler)
+	lambda.Start(Handler)
 }
